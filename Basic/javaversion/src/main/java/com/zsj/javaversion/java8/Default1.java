82f01df692202fe311c3ac77264a891c01c99341
zhaoshengjin <zhaoshengjin@shopuu.com>
2019-04-19 15:07:03 +0800

    java8特性

---
+++ Basic/javaversion/src/main/java/com/zsj/javaversion/java8/Default1.java
@@ -0,0 +1,8 @@
+package com.zsj.javaversion.java8;
+
+/**
+ * @author zsj
+ * @date 2019-04-19  11:19
+ */
+public interface Default1 {
+}
---
+++ Basic/javaversion/src/main/java/com/zsj/javaversion/java8/StreamTest.java
@@ -0,0 +1,11 @@
+package com.zsj.javaversion.java8;
+
+/**
+ * @author zsj
+ * @date 2019-04-19  11:57
+ */
+public class Stream {
+
+    public static void main(String[] args) {
+    }
+}
