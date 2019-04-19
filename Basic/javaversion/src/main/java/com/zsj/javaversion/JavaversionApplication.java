0bd866b125f99d43c05c0d0e2065ef19d94c3656
zhaoshengjin <zhaoshengjin@shopuu.com>
2019-04-19 10:40:55 +0800

    修改

---
+++ Basic/javaversion/src/main/java/com/zsj/javaversion/JavaversionApplication.java
@@ -0,0 +1,13 @@
+package com.zsj.javaversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaversionApplication.class, args);
+	}
+
+}
