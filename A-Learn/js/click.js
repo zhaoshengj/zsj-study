e7e07938c39c01d981fa00fa711c5bb547cee546
zhaoshengjin <zhaoshengjin@shopuu.com>
2019-04-28 13:36:13 +0800

    学习界面

--- A-Learn/js/click.js
+++ A-Learn/js/click.js
@@ -6,3 +6,16 @@ function showItem(menu){
         menu.style.display = "none";
     }
 }
+
+function show()  //显示隐藏层和弹出层
+{
+    container.style.display="block";  //显示隐藏层
+    container.style.height="50%";  //设置隐藏层的高度为当前页面高度
+    container.style.opacity=0.5;
+    document.getElementById("hidebox").style.display="block";  //显示弹出层
+}
+function hide()  //去除隐藏层和弹出层
+{
+    document.getElementById("hidebox").style.display="none";
+    container.style.opacity=1;
+}
--- A-doc/start.sh
+++ A-doc/start.sh
@@ -1,12 +1,14 @@
 echo '停止容器...'
-docker stop sentinel
+docker stop nginx
 echo '删除容器...'
-docker rm sentinel
-echo '删除镜像...'
-docker rmi sentinel:v1
-echo '构建镜像...'
-docker build -t sentinel:v1 .
+docker rm nginx
 echo '启动容器...'
-docker run -d --name sentinel -v /etc/localtime:/etc/localtime -v /opt/zsj/sentiel/logs/:/root/logs/ -p 8849:8080 sentinel:v1
+docker run --name nginx -d -p 80:80  -p 443:443 -v /opt/data/nginx/html:/usr/share/nginx/html \
+-v /opt/data/nginx/conf:/etc/nginx/conf \
+-v /opt/data/nginx/conf.d/default.conf:/etc/nginx/conf.d/default.conf \
+-v /opt/data/nginx/logs:/var/log/nginx \
+-v /opt/static:/opt/static \
+-v /etc/localtime:/etc/localtime \
+nginx-web
 echo '打印日志...'
-docker logs -f sentinel
+docker logs -f nginx
